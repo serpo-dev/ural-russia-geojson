9b9271f4b3b3f1bba4f95fd4a67c6ac470535769
Sergey <sergey.potapov.2002@mail.ru>
2023-07-31 15:19:09 +0500

    add only ural

---
+++ get_ural_level_4.py
@@ -0,0 +1,35 @@
+import json
+
+ural_areas = ["Курганскаяобласть", "Свердловскаяобласть", "Тюменскаяобласть", "Челябинскаяобласть", "Ханты-МансийскийАОк", "Ямало-НенецкийАОк"]
+
+EXTRACT_URAL_FILENAME = "input_1_save_ural.json"
+OUTPUT_FILENAME = "output_ural_level_4.json"
+
+# Extract Ural areas from the first file
+
+with open(EXTRACT_URAL_FILENAME, "r", encoding="utf-8") as input_to_save_ural_file:
+    to_save_ural_data = json.load(input_to_save_ural_file)
+
+features=to_save_ural_data["features"]
+ural_features = []
+for feature in features:
+    properties = feature["properties"]
+    name=properties["NL_NAME_1"]
+    if name in ural_areas:
+        ural_features.append(feature)
+
+# Comnine Ural and Non-Ural data and save topython output file
+
+with open(OUTPUT_FILENAME, "w", encoding="utf-8") as output_file:
+    data = {
+        "type":"FeatureCollection",
+        "name":"gadm41_RUS_3",
+        "crs":{
+            "type":"name",
+            "properties":{
+                "name":"urn:ogc:def:crs:OGC:1.3:CRS84"
+                }},
+                "features": ural_features 
+    }
+
+    json.dump(data, output_file, ensure_ascii=False)
